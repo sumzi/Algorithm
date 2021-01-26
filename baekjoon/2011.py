80ab70c67ce73ffb4ffd58c0fd948c4456e340f9
sumzi <wltn1873@gmail.com>
2021-01-26 14:39:59 +0900

    2011_암호코드

---
+++ baekjoon/2011.py
@@ -0,0 +1,13 @@
+n=input()
+dp=[0]*(len(n)+1)
+if n[0] == '0':
+    print(0)
+    exit()
+dp[0]=dp[1] = 1
+for i in range(2, len(n)+1):
+    if n[i-1] != '0':
+        dp[i] = dp[i-1]
+    tmp=int(n[i-2:i])
+    if 10<=tmp and tmp<=26:
+        dp[i]+=dp[i-2]
+print(dp[len(n)]%1000000)
