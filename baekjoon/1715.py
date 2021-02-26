1180c68b7fb49f10b8f0a0b69c16d90aff87a19f
sumzi <wltn1873@gmail.com>
2021-02-27 02:04:03 +0900

    solve heap

--- baekjoon/1715.py
+++ baekjoon/1715.py
@@ -2,12 +2,11 @@ import sys
 import heapq
 input=sys.stdin.readline
 n=int(input())
-q=[]
-for i in range(n):
-    heapq.heappush(q,int(input()))
-res=0
-while len(q)>1:
-    tmp=heapq.heappop(q)+heapq.heappop(q)
-    res+=tmp
-    heapq.heappush(q,tmp)
-print(res)
+q=list(map(int,input().split()))
+heapq.heapify(q)
+for i in range(n-1):
+    tmp=list(map(int,input().split()))
+    for j in tmp:
+        heapq.heappush(q,j)
+        heapq.heappop(q)
+print(heapq.heappop(q))
