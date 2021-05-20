e461d3e23d5eac63467fca7ce893e4fb95ba1ca4
sumzi <wltn1873@gmail.com>
2021-05-20 23:29:35 +0900

    Solve boj KMP

---
+++ baekjoon/1786.py
@@ -0,0 +1,27 @@
+t=input().replace('\n',"")
+p=input().replace('\n',"")
+n=len(t)
+m=len(p)
+fail=[0]*(m)
+
+j=0
+for i in range(1,m):
+    while j>0 and p[i]!=p[j]:
+        j=fail[j-1]
+    if p[i]==p[j]:
+        j+=1
+        fail[i]=j
+j=0
+ans=[]
+for i in range(n):
+    while j>0 and t[i]!=p[j]:
+        j=fail[j-1]
+    if t[i]==p[j]:
+        if j==m-1:
+            ans.append(i-m+2)
+            j=fail[j]
+        else:
+            j+=1
+print(len(ans))
+for i in ans:
+    print(i)
