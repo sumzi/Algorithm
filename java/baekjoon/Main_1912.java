fad51a1de73ef3417843498df460de7bb02e57f8
sumzi <wltn1873@gmail.com>
2021-07-19 22:51:59 +0900

    Add java folder

---
+++ java/baekjoon/Main_1912.java
@@ -0,0 +1,28 @@
+
+/**
+ * Main_1912
+ */
+import java.util.*;
+import java.io.*;
+
+public class Main_1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int sum = 0;
+        int ans = -1000;
+        for (int i = 0; i < n; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            if (tmp+sum<tmp){
+                sum=tmp;
+            }else{
+                sum+=tmp;
+            }
+            if (sum>ans){
+                ans=sum;
+            }
+        }
+        System.out.println(ans);
+    }
+}
