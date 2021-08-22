5d78743405b4187bfb149d8ec73369793478609f
sumzi <wltn1873@gmail.com>
2021-08-22 20:08:30 +0900

    Solve boj

---
+++ java/baekjoon/Brute_Force/Main1963.java
@@ -0,0 +1,58 @@
+package baekjoon.Brute_Force;
+
+import java.util.*;
+import java.io.*;
+
+public class Main1963 {
+    static boolean[] check = new boolean[10000];
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        for (int i = 2; i < 10000; i++) {
+            for (int j = i + i; j < 10000; j += i) {
+                check[j] = true;
+            }
+        }
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            visited = new boolean[10000];
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            Queue<int[]> q = new LinkedList<>();
+            q.add(new int[] { a, 0 });
+            visited[a] = true;
+            int ans = -1;
+            while (!q.isEmpty()) {
+                int[] x = q.poll();
+                int cur = x[0];
+                int cnt = x[1];
+                if (cur == b) {
+                    ans = cnt;
+                    break;
+                }
+                for (int j = 0; j < 4; j++) {
+                    for (int k = 0; k < 10; k++) {
+                        if (j == 0 && k == 0)
+                            continue;
+                        int tmp = change(cur, j, k);
+                        if (!check[tmp] && !visited[tmp]) {
+                            visited[tmp] = true;
+                            q.add(new int[] { tmp, cnt + 1 });
+                        }
+                    }
+                }
+            }
+            System.out.println(ans == -1 ? "Impossible" : ans);
+        }
+    }
+
+    public static int change(int num, int i, int j) {
+        StringBuilder sb = new StringBuilder(String.valueOf(num));
+        sb.setCharAt(i, (char) (j + '0'));
+        return Integer.parseInt(sb.toString());
+    }
+}
---
+++ java/baekjoon/Brute_Force/Main9019.java
@@ -0,0 +1,52 @@
+package baekjoon.Brute_Force;
+
+import java.util.*;
+import java.io.*;
+
+public class Main9019 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        boolean[] visited;
+        int t = Integer.parseInt(br.readLine());
+        int nx;
+        for (int i = 0; i < t; i++) {
+            visited = new boolean[10000];
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            visited[a] = true;
+            Queue<String[]> q = new LinkedList<>();
+            q.add(new String[] { String.valueOf(a), "" });
+            while (!q.isEmpty()) {
+                String[] tmp = q.poll();
+                int x = Integer.parseInt(tmp[0]);
+                String y = tmp[1];
+                if (x == b) {
+                    System.out.println(y);
+                    break;
+                }
+                nx = (x * 2) % 10000;
+                if (!visited[nx]) {
+                    visited[nx] = true;
+                    q.add(new String[] { String.valueOf(nx), y + 'D' });
+                }
+                nx = x != 0 ? x - 1 : 9999;
+                if (!visited[nx]) {
+                    visited[nx] = true;
+                    q.add(new String[] { String.valueOf(nx), y + 'S' });
+                }
+                nx = (x % 1000) * 10 + x / 1000;
+                if (!visited[nx]) {
+                    visited[nx] = true;
+                    q.add(new String[] { String.valueOf(nx), y + 'L' });
+                }
+                nx = (x % 10) * 1000 + x / 10;
+                if (!visited[nx]) {
+                    visited[nx] = true;
+                    q.add(new String[] { String.valueOf(nx), y + 'R' });
+                }
+            }
+        }
+    }
+}
